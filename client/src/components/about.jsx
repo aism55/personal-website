5ee04625a3d27d06b19d11f7b843c05207eca1dc
Shiyao Li <lishiyao221@gmail.com>
2018-06-21 16:09:07 -0700

    set up each page

---
+++ client/src/components/about.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => {
+  return ( 
+    <div>To do</div>
+  );
+}
+
+export default About;
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
+import About from './about.jsx';
+import Projects from './projects.jsx';
+import Contact from './contact.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentPage: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.renderPages = this.renderPages.bind(this);
+  }
+
+  handleClick(option) {
+    this.setState({
+      currentPage: option,
+    });
+  }
+
+  renderPages() {
+    if (this.state.currentPage === 'about') {
+      return <About />;
+    } else if (this.state.currentPage === 'projects') {
+      return <Projects />;
+    } else if (this.state.currentPage === 'contact') {
+      return <Contact />;
+    }
   }
 
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col s2">About</div>
-          <div className="col s2">Projects</div>
-          <div className="col s2">Contact</div>
+          <div className="col s2" onClick={() => this.handleClick('about')}>About</div>
+          <div className="col s2" onClick={() => this.handleClick('projects')}>Projects</div>
+          <div className="col s2" onClick={() => this.handleClick('contact')}>Contact</div>
+        </div>
+        <div>
+          {this.renderPages()}
         </div>
       </div>
     );
